74ebf159530872a36c8b69d25a6f8c291a5f83fb
Maxmilly4050 <maxsteel4050@gmail.com>
2026-03-29 00:59:54 +0300

    Setup of the places home screena nd the add new place screen

---
+++ lib/screens/add_place.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class AddPlaceScreen extends StatefulWidget {
+  const AddPlaceScreen({super.key});
+
+  @override
+  State<AddPlaceScreen> createState() => _AddPlaceScreenState();
+}
+
+class _AddPlaceScreenState extends State<AddPlaceScreen> {
+  final _titleController = TextEditingController();
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Add a New Place'),
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          children: [
+            TextField(
+              decoration: const InputDecoration(labelText: 'Title'),
+              controller: _titleController,
+            ),
+            const SizedBox(height: 10),
+            ElevatedButton.icon(onPressed: (){}, label: const Text('Add Place'), icon: const Icon(Icons.add)),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/places_list.dart
@@ -0,0 +1,35 @@
+import 'package:favorite_places/models/place.dart';
+import 'package:flutter/material.dart';
+
+class PlacesList extends StatelessWidget {
+  const PlacesList({super.key, required this.places});
+
+  final List<Place> places;
+
+  @override
+  Widget build(BuildContext context) {
+    if (places.isEmpty) {
+      return const Center(
+        child: Text('No places added yet.'),
+      );
+    }
+
+    return ListView.builder(
+      itemCount: places.length,
+      itemBuilder: (ctx, index) {
+        final place = places[index];
+        return ListTile(
+          // leading: CircleAvatar(
+          //   backgroundImage: FileImage(place),
+          // ),
+          title: Text(place.title, style: Theme.of(context).textTheme.titleMedium!.copyWith()),
+          // subtitle: Text(place.location.address),
+          onTap: () {
+            // Handle place tap
+          },
+        );
+      },
+    );
+
+  }
+}
